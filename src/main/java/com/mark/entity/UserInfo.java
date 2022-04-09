172f97888320c3f7ec81bfebd7ded17f45949514
hudonghui <234498142@qq.com>
2022-04-10 03:17:02 +0800

    1

--- src/main/java/com/mark/entity/UserInfo.java
+++ src/main/java/com/mark/entity/UserInfo.java
@@ -1,11 +1,13 @@
 package com.mark.entity;
 
-import com.baomidou.mybatisplus.annotation.*;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 
 import java.io.Serializable;
 import java.time.LocalDateTime;
-import java.util.Date;
 
 /**
  * ClassName:UserInfo
