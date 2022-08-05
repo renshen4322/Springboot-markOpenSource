92bacc5a30f1610e7875858422b7a8a93c4ee0b6
frog <234498142@qq.com>
2022-08-05 13:36:26 +0800

    SemaphoreLimit 999

--- src/main/java/com/mark/controller/HelloController.java
+++ src/main/java/com/mark/controller/HelloController.java
@@ -16,5 +16,23 @@ public class HelloController {
         return "id:"+id+ " name:"+name;
     }
 
+    /*@PostMapping(value = "/testRedis")
+    public R testRedis(HttpServletRequest request) {
+        // 通过用户ID和访问IP，处理重复请求
+        String key = userId + IPUtils.getIpAddr(request);
+        Object redisKey = redisTemplate.opsForValue().get(key);
+        System.out.println("redis值：" + redisKey);
+
+        // redis中的key值存在则表示当前这次请求距离上一次请求不超过下面设置的三秒钟，直接返回即可
+        if(redisKey != null){
+            return R.ok("您的操作过快，请刷新重试");
+        }
+
+        Long count = redisTemplate.opsForValue().increment(key, 1); // 设置递增因子
+        System.out.println("递增的值" + count);
+        redisTemplate.expire(key, 3, TimeUnit.SECONDS); // 设置过期时间 3秒
+        return R.ok("成功");
+    }
+*/
 
 }
